cc7b5fd04dcb87eb9bcd46b500e1feaded9a593c
Rojaraja <roja3024@gmail.com>
2018-09-12 20:51:21 +0530

    checking

--- final.java
+++ final.java
@@ -2,9 +2,11 @@ class Rose
 {
 static int a=10;
 static int b=20;
+static int c=30;
 public static void main(String[] args)
 {
 System.out.println("A="  +a);
+System.out.println("B="  +b);
 System.out.println("B="  +b);
 }
 }
